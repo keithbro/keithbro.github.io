c2c34348196311f04486d441c58e2ea95dbf45a5
Keith Broughton <keithbro256@gmail.com>
2017-12-28 15:17:32 +1100

    More SVGs

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,15 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import elasticsearch from '../images/elasticsearch.svg'
+import emberjs from '../images/emberjs.svg'
 import kinesis from '../images/kinesis.svg'
+import mysql from '../images/mysql.svg'
+import nodejs from '../images/nodejs.svg'
 import perl5 from '../images/perl5.svg'
 import postgresql from '../images/postgresql.svg'
 import rails from '../images/rails.svg'
 import ruby from '../images/ruby.svg'
+import vagrant from '../images/vagrant.svg'
 
 const IndexPage = () => (
   <div
@@ -15,13 +19,13 @@ const IndexPage = () => (
     }}
   >
     <h1>Keith Broughton</h1>
-    <p>Developer | Sydney, Australia</p>
+    <h4>Developer in Sydney, Australia</h4>
 
     <hr/>
 
     <h2>Current Position</h2>
+    <h4>Developer @ Fat Zebra / Cloud Payments (August 2017 - Present)</h4>
 
-    <p>Developer | Fat Zebra / Cloud Payments | Sydney, Australia</p>
     <img className="logo" src={ruby}/>
     <img className="logo" src={rails}/>
     <img className="logo" src={elasticsearch}/>
@@ -30,11 +34,19 @@ const IndexPage = () => (
     <hr/>
 
     <h2>Previous Positions</h2>
+    <h4>Senior Developer / Tech Lead @ YOOX NET-A-PORTER Group (2016/17)</h4>
 
-    <p>Senior Developer / Tech Lead | YOOX NET-A-PORTER Group | London, United Kingdom</p>
     <img className="logo" src={perl5}/>
     <img className="logo" src={postgresql}/>
     <img className="logo" src={kinesis}/>
+    <img className="logo" src={nodejs}/>
+
+    <h4>Senior Developer @ Broadbean Technology (2014 - 2016)</h4>
+    <img className="logo" src={perl5}/>
+    <img className="logo" src={mysql}/>
+    <img className="logo" src={emberjs}/>
+    <img className="logo" src={vagrant}/>
+
   </div>
 )
 
