8e2ea5312e14031fda1d5dbdae0ef0943f42178c
Keith Broughton <keithbro256@gmail.com>
2017-12-28 15:00:58 +1100

    All SVGs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
+
+import elasticsearch from '../images/elasticsearch.svg'
 import kinesis from '../images/kinesis.svg'
+import perl5 from '../images/perl5.svg'
 import postgresql from '../images/postgresql.svg'
+import rails from '../images/rails.svg'
+import ruby from '../images/ruby.svg'
 
 const IndexPage = () => (
   <div
@@ -17,9 +22,9 @@ const IndexPage = () => (
     <h2>Current Position</h2>
 
     <p>Developer | Fat Zebra / Cloud Payments | Sydney, Australia</p>
-    <img className="logo" src="https://avatars2.githubusercontent.com/u/210414?s=200&v=4"/>
-    <img className="logo" src="https://pbs.twimg.com/profile_images/691206086955790336/CDMbA57p_400x400.png"/>
-    <img className="logo" src="https://pbs.twimg.com/profile_images/573291480862744576/LP8JoaZ4_400x400.png"/>
+    <img className="logo" src={ruby}/>
+    <img className="logo" src={rails}/>
+    <img className="logo" src={elasticsearch}/>
     <img className="logo" src={postgresql}/>
 
     <hr/>
@@ -27,7 +32,7 @@ const IndexPage = () => (
     <h2>Previous Positions</h2>
 
     <p>Senior Developer / Tech Lead | YOOX NET-A-PORTER Group | London, United Kingdom</p>
-    <img className="logo" src="https://uploads.toptal.io/blog/category/logo/48/perl.png"/>
+    <img className="logo" src={perl5}/>
     <img className="logo" src={postgresql}/>
     <img className="logo" src={kinesis}/>
   </div>
